e04685a4c91b921c8c2626a660721ccff641c30d
bilalmisla <bilal@madeinsouth.la>
2024-12-19 01:30:08 +0700

    expiresIn

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -58,7 +58,7 @@ const authLogin = async (request, response) => {
             const token = jwt.sign({
                 _id: user._id,
                 isSeller: user.isSeller
-            }, JWT_SECRET, { expiresIn: '7 days' });
+            }, JWT_SECRET, { expiresIn: '7d' });
 
             const cookieConfig =  {
                 httpOnly: true,
