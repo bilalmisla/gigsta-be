813b1fdd4e6f2d42af7b7b457a8a84abc423ed8b
bilalmisla <bilal@madeinsouth.la>
2024-12-23 21:10:07 +0500

    throw request error from authenticate method

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -7,7 +7,7 @@ const authenticate = (request, response, next) => {
     
     try {
         if (!accessToken) {
-            throw CustomException('Access denied!', 401);
+            throw CustomException(request, 401);
         }
 
         const verification = jwt.verify(accessToken, process.env.JWT_SECRET);
