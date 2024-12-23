714498b89f5393326fd6502bae005b43c8c91a2b
bilalmisla <bilal@madeinsouth.la>
2024-12-23 20:42:45 +0500

    change authorization key in response header

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -19,7 +19,7 @@ app.get('/me', authenticate, authStatus);
 
 // Middleware to verify token
 const authenticateToken = async (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+    const token = req.headers['Authorization']?.split(' ')[1];
     if (!token) return res.status(401).json({ error: 'Token is required' });
 
     try {
