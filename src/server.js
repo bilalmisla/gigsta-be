c2b298a603f38fcaaef6ac5d09a71d5b4742e017
bilalmisla <bilal@madeinsouth.la>
2024-12-24 15:16:00 +0700

    update json and port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const compression = require('compression');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connect = require('./configs/db');
-const PORT = 3000;
+const PORT = 8080;
 
 // Other Route files
 const { userRoute, conversationRoute, gigRoute, messageRoute, orderRoute, reviewRoute, authRoute } = require('./routes');
