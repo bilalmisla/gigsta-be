87edf2a92f03a8b5165734fa2425c3e25a594ec3
bilalmisla <bilal@madeinsouth.la>
2024-12-25 16:22:48 +0700

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -62,8 +62,8 @@ const authLogin = async (request, response) => {
 
             const cookieConfig = {
                 httpOnly: true,
-                sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none', // Important for cross-site cookies
-                secure: process.env.NODE_ENV === 'production', // Must be true in production (requires HTTPS)
+                sameSite: 'none', // Important for cross-site cookies
+                secure: NODE_ENV === 'production', // Must be true in production (requires HTTPS)
                 maxAge: 60 * 60 * 24 * 7 * 1000, // 7 days
                 path: '/'
             };
