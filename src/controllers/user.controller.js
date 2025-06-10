020640e24d47d1e1879e39e217b982ee25a80bb9
bilalmisla <bilal@madeinsouth.la>
2025-06-10 16:04:52 +0500

    update method for fetching top sellers accounts in homepage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Order } = require('../models');
 const { CustomException } = require('../utils');
 
 const deleteUser = async (request, response) => {
@@ -27,10 +27,47 @@ const deleteUser = async (request, response) => {
 
 const fetchTopSellers = async (request, response) => {
     try {
-        // Fetch only users who are sellers and verified (soft deletion is automatically handled by pre hook)
-        const topSellers = await User.find({ isSeller: true, isVerified: true })
-            .sort({ createdAt: -1 }) // or sort by rating or totalSales if you add those
-            .limit(10); // to limit the result to top 10
+        const topSellers = await Order.aggregate([
+            { $match: { deletedAt: null } }, // Ignore soft-deleted orders
+            { $unwind: "$gigs" },
+            {
+                $group: {
+                    _id: "$gigs.sellerID",
+                    totalOrders: { $sum: 1 }
+                }
+            },
+            { $sort: { totalOrders: -1 } },
+            { $limit: 10 }, // Optional: top 10 sellers
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "seller"
+                }
+            },
+            { $unwind: "$seller" },
+            {
+                $match: {
+                    "seller.isSeller": true,
+                    "seller.isVerified": true,
+                    "seller.deletedAt": null
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    sellerID: "$_id",
+                    totalOrders: 1,
+                    username: "$seller.username",
+                    email: "$seller.email",
+                    country: "$seller.country",
+                    image: "$seller.image",
+                    description: "$seller.description"
+                }
+            }
+        ]);
+
 
         return response.send({
             error: false,
